678f2315a26f1b1900dc7466e75e3c643a2c2e91
Markus Penttila <markus.penttila@cloudpermit.com>
2023-02-24 20:28:09 -0500

    Add EUPL 1.2 license, Clojars deploy config

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Common utilities for constructing Google Cloud Pub/Sub components in Lupapiste a
 
 ## License
 
-Copyright © 2021 Cloudpermit Oy
+Copyright © 2023 Cloudpermit Oy
+
+Distributed under the European Union Public Licence (EUPL) version 1.2.
--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
-(defproject lupapiste/pubsub-client "2.5.0"
+(defproject fi.lupapiste/pubsub-client "2.5.1"
   :description "Common GCP Pub/Sub utils for Lupapiste"
-  :url "https://github.com/cloudpermit/lupapiste-pubsub"
+  :url "https://github.com/lupapiste/lupapiste-pubsub"
+  :license {:name         "European Union Public Licence v. 1.2"
+            :url          "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
+            :distribution :manual}
   :dependencies [[com.google.cloud/google-cloud-core "2.5.11"]
                  [com.google.cloud/google-cloud-pubsub "1.116.3"]
                  [com.taoensso/timbre "5.2.1"]
                  [org.clojure/tools.reader "1.3.6"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]]}}
-  :deploy-repositories [["releases" {:url           "https://maven.pkg.github.com/cloudpermit/lupapiste-pubsub"
-                                     :username      :env/cloudpermit_github_username
-                                     :password      :env/cloudpermit_github_token
-                                     :sign-releases false}]]
+  :deploy-repositories [["clojars" {:username      :env/clojars_username
+                                    :password      :env/clojars_token
+                                    :sign-releases false}]]
   )
