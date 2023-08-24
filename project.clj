28b14d6665593c7893a14ae2f0984cbfdae0274a
Markus Penttila <markus.penttila@cloudpermit.com>
2023-08-24 16:11:39 -0400

    Upgrade GCP dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject fi.lupapiste/pubsub-client "2.5.2"
+(defproject fi.lupapiste/pubsub-client "2.5.3"
   :description "Common GCP Pub/Sub utils for Lupapiste"
   :url "https://github.com/lupapiste/lupapiste-pubsub"
   :license {:name         "European Union Public Licence v. 1.2"
             :url          "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
             :distribution :manual}
-  :dependencies [[com.google.cloud/google-cloud-core "2.18.1"]
-                 [com.google.cloud/google-cloud-pubsub "1.123.13"]
+  :dependencies [[com.google.cloud/google-cloud-core "2.22.0"]
+                 [com.google.cloud/google-cloud-pubsub "1.124.1"]
                  [com.taoensso/timbre "6.1.0"]
                  [org.clojure/tools.reader "1.3.6"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}}
